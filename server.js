330f5cb6782a7e843d9d766f621629d672c97e98
Itsnope <noverjkd@gmail.com>
2025-06-17 18:41:11 +0700

    feat: sesi2 buat server & halaman HTML web chat

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Server Web Chat Berjalan!</h1>');
-});
+// Menyajikan file statis dari folder "public".
+  // Misal, ada index.html di public, maka index.html akan disajikan sbg tampilan dari  http://localhost:3000/
+  // Nama file statis tdk akan masuk ke struktur URL.
+app.use(express.static('public'));
 
+// Kode dijalankan setiap kali ada client/pengguna yg terhubung ke server.
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
+  // 1. Terima pesan dari client.
+    // Pesan ini dikirim dari klien melalui socket.emit('chat message', msg);
+  socket.on('chat message', (msg) => {
+    console.log('Pesan diterima:', msg);
+    // 2. Kirim pesan ke semua client lain yg terhubung dengan server.
+    io.emit('chat message', msg); 
+  });
+
+  // Kode dijalankan ketika client/pengguna terputus dari server (misal: user menutup tab, koneksi internet putus, atau refresh halaman).
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
   });
